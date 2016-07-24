d6723e98c4abaa7fc26d69e0d4e4473588211069
Mateus Marques <marques.m05@live.com>
2016-07-24 14:19:22 -0300

    estilizando as views

--- routes/users.js
+++ routes/users.js
@@ -4,6 +4,6 @@ var service = require('./../services/user');
 
 /* GET users listing. */
 router.get('/login', service.login);
-router.get('/register', service.login);
+router.get('/register', service.register);
 
 module.exports = router;
